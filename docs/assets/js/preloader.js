efc8b783bd6eb424881a02a21584fa62c779cb6e
Vlad Denisov <ipovlad.yt@gmail.com>
2017-04-03 22:32:47 +0300

    Add docs folder for live preview

---
+++ docs/assets/js/preloader.js
@@ -0,0 +1,9 @@
+document.body.onload = function(){
+  setTimeout(function() {
+    var preloader = document.getElementById('loader');
+    if( !preloader.classList.contains('done') )
+    {
+      preloader.classList.add('done');
+    }
+  }, 900)
+}
